288695a72c15a9cd703555e773178a71428e7d9a
rodripsa <rodrigo.padilha@marcopolo.com.br>
2020-02-26 13:22:12 -0300

    Implementado classe registro boas práticas para privacidade de variáveis

--- js/app/models/Registro.js
+++ js/app/models/Registro.js
@@ -1,12 +1,39 @@
 class Registro{
 
-    constructor(){
-        this._alimentacao = ''
-        this._exercicio = ''
-        this._intestino = ''
-        this._sono = 0
-        this._agua = 0.0
-        this._excecoes = ''
+    constructor(alimentacao, exercicio, intestino, sono, agua, excecoes){
+        this._alimentacao = alimentacao
+        this._exercicio = exercicio
+        this._intestino = intestino
+        this._sono = sono
+        this._agua = agua
+        this._excecoes = excecoes
+    }
+
+    get alimentacao(){
+        return this._alimentacao
+    }
+    // set alimentacao(data){
+    //     this._alimentacao = data
+    // }
+
+    get exercicio() {
+        return this._exercicio
+    }
+
+    get intestino() {
+        return this._intestino
+    }
+
+    get sono() {
+        return this._sono
+    }
+
+    get agua() {
+        return this._agua
+    }
+
+    get excecoes() {
+        return this._excecoes
     }
 
 }
