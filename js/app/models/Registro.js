713ca332b14c61c2420afede6eb51ab05772d1af
rodripsa <rodrigo.padilha@marcopolo.com.br>
2020-02-26 08:21:34 -0300

    Primeiro commit

---
+++ js/app/models/Registro.js
@@ -0,0 +1,12 @@
+class Registro{
+
+    constructor(){
+        this._alimentacao = ''
+        this._exercicio = ''
+        this._intestino = ''
+        this._sono = 0
+        this._agua = 0.0
+        this._excecoes = ''
+    }
+
+}
