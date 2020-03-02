aab87db1a120fb326def7861b2eb884ab0dd2fb7
rodripsa <rodrigo.padilha@marcopolo.com.br>
2020-03-02 13:43:47 -0300

    Implementado conversão de input date para objeto JS Date Ajustar esse COMMIT

--- js/app/controllers/RegistroController.js
+++ js/app/controllers/RegistroController.js
@@ -29,12 +29,12 @@ class RegistroController{
 
         //Alura aula 3 - criando um objeto Date
 
-        let data = new Date(...
-            this._currentDate.value
-            .split('-')
-            .map((item, indice) => {
-                return item - indice % 2;
-            })
+        let data = new Date(...             // Spread operator passa array como parametro sendo que
+            this._currentDate.value         // o primerio item do array será o primeiro parametro,
+            .split('-')                     // o segundo item do array será o segundo parametros, ...
+            .map((item, indice) => {    
+                return item - indice % 2;   // Converte data do formato 2020-03-29 para
+            })                              // Sun Mar 29 2020 00:00:00 GMT-0300 (Horário Padrão de Brasília)
         );
 
         let registro = new Registro(data,"2","3","4","5","6")
