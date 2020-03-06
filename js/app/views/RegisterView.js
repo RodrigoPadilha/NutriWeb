3a302e5ed374713ee724d23fb49fa74502f55c48
rodripsa <rodrigo.padilha@marcopolo.com.br>
2020-03-06 14:03:29 -0300

    Finalizado classe que renderiza itens na tabela assim que incluído novo registro

--- js/app/views/RegisterView.js
+++ js/app/views/RegisterView.js
@@ -11,35 +11,39 @@ class RegisterView{
                     <tr>
                         <th scope="col">#</th>
                         <th scope="col">Data</th>
+                        <th scope="col">Alimentação</th>
                         <th scope="col">Exercício</th>
                         <th scope="col">Intestino</th>
+                        <th scope="col">Sono</th>
+                        <th scope="col">Água</th>
+                        <th scope="col">Exceções</th>
                     </tr>
                 </thead>
                 <tbody>
-
                     ${model.registers.map(register => {
                         return `
                             <tr>
                                 <th scope="row">1</th>
                                 <td>${DateHelper.convertDateToText(register.date)}</td>
+                                <td>${register.food.reduce((text, f) => text+=`${f.key}:${f.value} `,"")}</td> 
                                 <td>${register.exercise}</td>
                                 <td>${register.intestine}</td>
+                                <td>${register.sleep}</td>
+                                <td>${register.water}</td>
+                                <td>${register.exceptions}</td>
                             </tr>
                         `    
-                    }).join('')}
-
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                    </tr>
+                    }).join('')}                    
                 </tbody>
             </table>
         `
     }
 
     update(model){
-        this._elemento.innerHTML = this._template(model);
+        /** 
+         * Qualquer texto HTML válido inserido para o atributo innerHTML é convertido para elementos do DOM
+         * e renderizados na página
+         *  */ 
+        this._elemento.innerHTML = this._template(model);       
     }
 }
